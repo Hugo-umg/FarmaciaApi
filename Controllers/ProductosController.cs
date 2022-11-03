401aa7aafbe4a29d119bfa499aee8c7c5321214b
hrvil <hrvil@LAPTOP-NVU10871>
2022-11-02 19:50:43 -0600

    Add project files.

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,40 @@
+﻿using FarmaciaAPI.Data.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FarmaciaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private readonly IProductoRepository _productoRepository;
+
+        public ProductosController(IProductoRepository productoRepository)
+        {
+            _productoRepository = productoRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductos()
+        {
+            return Ok(await _productoRepository.GetProductos(0));
+        }
+
+        [HttpGet("{idProducto}")]
+        public async Task<IActionResult> GetProducto(int idProducto)
+        {
+            return Ok(await _productoRepository.GetProducto(idProducto));
+        }
+
+        [HttpGet("{idSucursal}, {idCategoria}")]
+        public async Task<IActionResult> GetProductosCategoria(int idSucursal, int idCategoria)
+        {
+            return Ok(await _productoRepository.GetProductosCategoria(idSucursal, idCategoria));
+        }
+    }
+}
